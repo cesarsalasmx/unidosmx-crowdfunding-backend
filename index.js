fdf8373825e16b539d610912b7c7c2540b368efc
Cesar Salas <contacto@cesarsalas.mx>
2022-04-29 04:31:32 -0500

    Setup to production

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const query = require("./graphql/resolvers/queries");
 const mutation = require("./graphql/resolvers/mutations");
 const schema = new GraphQLSchema({ query: query, mutation: mutation });
 const cors = require("cors");
-
+require("dotenv").config();
+const port = process.env.PORT || 4000;
 const app = express();
 app.use('/graphql', cors(),graphqlHTTP({
   schema: schema,
   graphiql: true,
 }));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+app.listen(port);
+console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
